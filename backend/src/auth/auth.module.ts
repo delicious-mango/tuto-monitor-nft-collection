e291a44884690452195f9543532432eb9e963dd4
Alexandre Schaffner <alexandre.s@starton.com>
2023-05-03 09:44:20 +0200

    refactor: exports / imports of auth.module and user.module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from 'src/user/user.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [
@@ -11,9 +12,9 @@ import { JwtModule } from '@nestjs/jwt';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
-    UserModule,
   ],
   controllers: [AuthController],
-  providers: [PrismaService, AuthService],
+  providers: [PrismaService, AuthService, AuthGuard, UserService],
+  exports: [AuthService, JwtModule, AuthGuard],
 })
 export class AuthModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthGuard } from 'src/auth/auth.guard';
-import { JwtService } from '@nestjs/jwt';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserController],
-  providers: [PrismaService, UserService, AuthGuard, JwtService],
-  exports: [UserService],
+  providers: [PrismaService, UserService],
 })
 export class UserModule {}
